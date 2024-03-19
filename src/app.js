526e3d0a46a52ec61dd870136c00debc82a1018f
Nakod55 <nakodvenkatesh43@gmail.com>
2024-03-19 21:29:06 +0530

    submit

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const app = express();
 
 app.get('/product/:productId', (req, res) => {
   //Write a code here for endpoint /product/:productId and also print parameter in json form
+     res.send({productId: req.params.productId});
 });
 
 module.exports = app;
